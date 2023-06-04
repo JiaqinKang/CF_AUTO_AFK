78c9ce2a0cd70895d0db3fa605652d0b30b1e4ad
JiaqinKang <qq529879477@gmail.com>
2023-06-04 01:00:18 +0100

    fix

--- cf/cf.py
+++ cf/cf.py
@@ -1,79 +1,120 @@
-import pyautogui
+import threading
+import keyboard
 import time
-from multiprocessing import Pool
-import gc
-import keyboard 
 
-# Define the image filenames to search for
-images_to_search = [ 
-                    'blacklist.png',
-                    'confirm.png', 
-                    'runaway.png','cancel2.png',
-                    'join.png','ready.png', 'ok.png', 
-                    'ok1.png', 'ok2.png', 'mpOK.png',  
-                    'achievement.png',  'close.png', 
-                    'close2.png','levelup.png','error.png'
-                    ]
+# This variable will keep track of whether the scripts are paused
+paused = True
 
-# Define the action to perform when the image is found
-action = 'click'
-click_coords = (100, 200)  # Coordinates for mouse click
+def toggle_scripts_state():
+    global paused
+    paused = not paused
 
-pause = True  # Control variable
+# Define the action that you want to take place
+def type_s():
+    keyboard.press('s')
+    time.sleep(0.5)
+    keyboard.release('s')
+    keyboard.press('a')
+    time.sleep(0.25)
+    keyboard.release('a')
 
-def find_and_click(image):
-    # Search for the image on the screen
-    loc = pyautogui.locateOnScreen(image, confidence=0.75)
-    if loc is not None:
-        print(f'{image} found')
-        if action == 'click':
-            
-            # Get the center coordinates of the image
-            center = pyautogui.center(loc)
-            if image == 'blacklist.png':
-                pyautogui.click(center)  # click
-                pyautogui.click(center)  # twice
-                time.sleep(1)
-                
-            # Check if the image is 'error.png'
-            elif image == 'error.png':
-                pyautogui.click(center)  # click once
-                pyautogui.click(center)  # click once
-                keyboard.press('esc')
-                keyboard.release('esc')
-                time.sleep(1)
-                keyboard.press('enter')
-                keyboard.release('enter')
-                time.sleep(1)
-                keyboard.press('enter')
-                keyboard.release('enter')
-                time.sleep(15)
-                return
-            
-            else:
-                # Perform the click action on the center coordinates
-                pyautogui.click(center)  # click once
-    gc.collect()
+def run_cf_script():
+    import pyautogui
+    import time
+    from multiprocessing import Pool
+    import gc
 
-def start_detection(e):
-    global pause
-    pause = False
-    print("Detection started")
+    # Define the image filenames to search for
+    images_to_search = [
+        'blacklist.png',
+        'confirm.png',
+        'runaway.png',
+        'cancel2.png',
+        'join.png',
+        'ready.png',
+        'ok.png',
+        'ok1.png',
+        'ok2.png',
+        'mpOK.png',
+        'achievement.png',
+        'close.png',
+        'close2.png',
+        'levelup.png',
+        'error.png'
+    ]
 
-def stop_detection(e):
-    global pause
-    pause = True
-    print("Detection paused")
+    # Define the action to perform when the image is found
+    action = 'click'
 
-if __name__ == "__main__":
-    # Register hotkeys
-    keyboard.on_press_key("f10", start_detection)
-    keyboard.on_press_key("f12", stop_detection)
+    def find_and_click(image):
+        # Search for the image on the screen
+        loc = pyautogui.locateOnScreen(image, confidence=0.75)
+        if loc is not None:
+            # print(f'{image} found')
+            if action == 'click':
+                # Get the center coordinates of the image
+                center = pyautogui.center(loc)
+                if image == 'blacklist.png':
+                    pyautogui.click(center)  # click
+                    pyautogui.click(center)  # twice
+                    time.sleep(1)
+                elif image == 'error.png':
+                    pyautogui.click(center)  # click once
+                    pyautogui.click(center)  # click once
+                    keyboard.press('esc')
+                    keyboard.release('esc')
+                    time.sleep(1)
+                    keyboard.press('enter')
+                    keyboard.release('enter')
+                    time.sleep(1)
+                    keyboard.press('enter')
+                    keyboard.release('enter')
+                    time.sleep(15)
+                    return
+                else:
+                    # Perform the click action on the center coordinates
+                    pyautogui.click(center)  # click once
+        gc.collect()
 
-    # Start the multiprocessing pool
-    pool = Pool(processes=1)
+    def start_detection(e):
+        global paused
+        paused = False
+        print("Detection started")
 
+    def stop_detection(e):
+        global paused
+        paused = True
+        print("Detection paused")
+
+    if __name__ == "__main__":
+        # Register hotkeys
+        keyboard.on_press_key("f10", start_detection)
+        keyboard.on_press_key("f12", stop_detection)
+
+        while True:
+            if not paused:
+                for image in images_to_search:
+                    find_and_click(image)
+            time.sleep(0.1)  # This prevents your while loop from hogging all the CPU power
+
+def run_move_script():
     while True:
-        if not pause:
-            pool.map(find_and_click, images_to_search)
-        time.sleep(0.1)  # This prevents your while loop from hogging all the CPU power
+        if not paused:
+            type_s()
+        time.sleep(0.1)
+
+# Create the threads
+thread1 = threading.Thread(target=run_cf_script)
+thread2 = threading.Thread(target=run_move_script)
+
+# Register hotkeys to control both scripts together
+keyboard.on_press_key('f10', lambda _: toggle_scripts_state())
+keyboard.on_press_key('f12', lambda _: toggle_scripts_state())
+
+# Start the threads
+thread1.start()
+thread2.start()
+
+# Wait for both threads to finish
+thread1.join()
+thread2.join()
--- cf/test.py
+++ cf/test.py
@@ -1,20 +1,120 @@
-import ctypes
+import threading
 import keyboard
+import time
 
+# This variable will keep track of whether the scripts are paused
+paused = True
 
-def run_test():
-    # use window api to move the mouse up 10 pixels
-    ctypes.windll.user32.mouse_event(0x0001, 0, -10, 0, 0)
-    # use window api to move the mouse down 10 pixels
-    
-# Callback function for F10 key press
-def on_key_press(event):
-    if event.name == 'f10':
-        print('f10 pressed')
-        run_test()
+def toggle_scripts_state():
+    global paused
+    paused = not paused
 
-# Register the callback function for key press events
-keyboard.on_press(on_key_press)
+# Define the action that you want to take place
+def type_s():
+    keyboard.press('s')
+    time.sleep(0.5)
+    keyboard.release('s')
+    keyboard.press('a')
+    time.sleep(0.25)
+    keyboard.release('a')
 
-# Keep the script running until interrupted
-keyboard.wait('esc')
+def run_cf_script():
+    import pyautogui
+    import time
+    from multiprocessing import Pool
+    import gc
+
+    # Define the image filenames to search for
+    images_to_search = [
+        'blacklist.png',
+        'confirm.png',
+        'runaway.png',
+        'cancel2.png',
+        'join.png',
+        'ready.png',
+        'ok.png',
+        'ok1.png',
+        'ok2.png',
+        'mpOK.png',
+        'achievement.png',
+        'close.png',
+        'close2.png',
+        'levelup.png',
+        'error.png'
+    ]
+
+    # Define the action to perform when the image is found
+    action = 'click'
+
+    def find_and_click(image):
+        # Search for the image on the screen
+        loc = pyautogui.locateOnScreen(image, confidence=0.75)
+        if loc is not None:
+            # print(f'{image} found')
+            if action == 'click':
+                # Get the center coordinates of the image
+                center = pyautogui.center(loc)
+                if image == 'blacklist.png':
+                    pyautogui.click(center)  # click
+                    pyautogui.click(center)  # twice
+                    time.sleep(1)
+                elif image == 'error.png':
+                    pyautogui.click(center)  # click once
+                    pyautogui.click(center)  # click once
+                    keyboard.press('esc')
+                    keyboard.release('esc')
+                    time.sleep(1)
+                    keyboard.press('enter')
+                    keyboard.release('enter')
+                    time.sleep(1)
+                    keyboard.press('enter')
+                    keyboard.release('enter')
+                    time.sleep(15)
+                    return
+                else:
+                    # Perform the click action on the center coordinates
+                    pyautogui.click(center)  # click once
+        gc.collect()
+
+    def start_detection(e):
+        global paused
+        paused = False
+        print("Detection started")
+
+    def stop_detection(e):
+        global paused
+        paused = True
+        print("Detection paused")
+
+    if __name__ == "__main__":
+        # Register hotkeys
+        keyboard.on_press_key("f10", start_detection)
+        keyboard.on_press_key("f12", stop_detection)
+
+        while True:
+            if not paused:
+                for image in images_to_search:
+                    find_and_click(image)
+            time.sleep(0.1)  # This prevents your while loop from hogging all the CPU power
+
+def run_move_script():
+    while True:
+        if not paused:
+            type_s()
+        time.sleep(0.1)
+
+# Create the threads
+thread1 = threading.Thread(target=run_cf_script)
+thread2 = threading.Thread(target=run_move_script)
+
+# Register hotkeys to control both scripts together
+keyboard.on_press_key('f10', lambda _: toggle_scripts_state())
+keyboard.on_press_key('f12', lambda _: toggle_scripts_state())
+
+# Start the threads
+thread1.start()
+thread2.start()
+
+# Wait for both threads to finish
+thread1.join()
+thread2.join()
